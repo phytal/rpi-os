a450d47b434d09a764ba76f59f301d2623e90fe8
William Zhang <willzhang058@gmail.com>
2024-12-09 22:28:18 -0600

    remove unnecessary libs

--- diddy-os/stddef.h
+++
@@ -1,9 +0,0 @@
-typedef unsigned int size_t;
-typedef unsigned int uint32_t;
-typedef int int32_t;
-typedef unsigned int uintptr_t;
-
-typedef int int32_t;
-typedef char int8_t;
-typedef unsigned long uint64_t;
-typedef unsigned char uint8_t;
